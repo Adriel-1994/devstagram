9775516e839e4069a04da8a5ae4d7c50f7c9488e
Adriel García Díaz <adriel.garcia102@gmail.com>
2023-04-30 19:22:50 +0100

    UPD in LoginController for send the auth username. Create a function in posts controller to create new post in the user wall. Add the Create button in app.blade for create a new post. New functionality to create a new post in the navbar using Dropzone library. Add the app.js in the app.blade to use this Dropzone custom library.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import Dropzone from "dropzone";
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone( '#dropzone', {
+    dictDefaultMessage: 'Sube aqui tu imagen',
+    acceptedFiles: '.pnh, .jpg, .jpeg, .gif',
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo',
+    maxFiles: 1,
+    uploadMultiple: false
+} )
