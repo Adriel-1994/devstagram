8f13c537eb4fb1a32732f1be110f86dbb9083e79
Adriel García Díaz <adriel.garcia102@gmail.com>
2023-05-01 10:29:50 +0100

    Update ImagenController to fit image with InterventionImage. Create model, factory and migration to Class Post. Create the form validation to upload an image.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,7 +16,7 @@
             </form>
         </div>
         <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
-            <form action="{{route('register')}}" method="POST" novalidate>
+            <form action="{{route('posts.store')}}" method="POST" novalidate>
                 @csrf 
                 <div class="mb-5 ">
                     <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">Titulo</label>
@@ -27,6 +27,10 @@
                     <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">Descripción</label>
                     <textarea id="descripcion" name="descripcion" placeholder="Descripción de la publicación" class="border p-3 w-full rounded-lg @error('descripcion') border-red-500 @enderror">{{old('descripcion')}}</textarea>
                     @error('descripcion') <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p> @enderror
+                </div>
+                <div class="mb-5">
+                    <input name="imagen" type="hidden" value="{{ old( 'imagen' ) }}"/>
+                    @error('imagen') <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p> @enderror
                 </div>
                 <input type="submit" value="Publicar" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"/>
             </form>
