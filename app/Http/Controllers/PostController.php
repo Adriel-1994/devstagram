2aa4240bd0bdd1e176837cdd4abbadedcaa26423
Adriel García Díaz <adriel.garcia102@gmail.com>
2023-05-01 10:43:44 +0100

    Create the function to save in bbdd the post's information

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -27,5 +28,22 @@ class PostController extends Controller
             'descripcion' => 'required|max:1000',
             'imagen' => 'required'
         ]);
+
+        Post::create( [
+            'titulo' => $request->titulo ,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ] );
+
+        // otra forma de crear
+        // $post = new Post();
+        // $post->titulo = $request->titulo;
+        // $post->descripcion = $request->descripcion;
+        // $post->imagen = $request->imagen;
+        // $post->user_id = auth()->user()->id;
+        // $post->save();
+        
+        return redirect()->route( 'posts.index', auth()->user()->username );
     }
 }
